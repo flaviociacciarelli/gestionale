f0529e9bcf76d85f90e13a09ca0d3faa767c2feb
Flory26 <diaconuflory26@gmal.com>
2025-07-22 10:13:43 +0200

    upgrade login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,4 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+/*document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const user = document.getElementById("usernameLogin").value;
@@ -23,6 +23,37 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
         console.log("non loggati");
       }
     });
+
+  });*/
+
+  document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const user = document.getElementById("usernameLogin").value;
+  const password = document.getElementById("passwordLogin").value;
+
+  fetch("/assets/db/login.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Impossibile caricare gli utenti");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const utente = data.utenti.find(
+        (u) => u.username === user && u.password === password
+      );
+
+      if (utente) {
+        console.log("Login riuscito");
+        // Reindirizza alla pagina Studente
+        window.location.href = "aggiungiStudenti.html";
+      } else {
+        alert("Credenziali errate");
+      }
+    })
+    .catch((error) => {
+      console.error("Errore:", error);
+      alert("Errore nel caricamento dei dati");
+    });
 });
-
-
